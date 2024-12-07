b625a8f7063cac20f21bed4ec896e04b047fdbd1
Christophe Thibaut <cthibauttof@gmail.com>
2024-12-07 14:46:22 +0100

    hook

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ spike: spike.s run_mos6502.cpp
 	mkdir -p bin
 	xa spike.s -o bin/spike.bin
 	bin2c bin/spike.bin -o run_memory.h
-	cc run_mos6502.cpp mos6502.cpp -o bin/run 
+	cc -std=c++11 run_mos6502.cpp mos6502.cpp -o bin/run 
--- run_mos6502.cpp
+++ run_mos6502.cpp
@@ -7,6 +7,8 @@ uint16_t origin = 0x0200;
 
 uint8_t memory[0x10000];
 
+char buffer[256];
+
 void init_memory() {
     unsigned index;
     for(index = 0; index < 0x10000; memory[index++] = 0);
@@ -18,19 +20,26 @@ void init_memory() {
 }
 
 uint8_t MemoryRead(uint16_t address) {
+    if (address == 0xFFFE) {
+        exit(0);
+    }
     return memory[address];
 }
 
+void ClockCycle(mos6502 *mpu) {
+    mpu->PrintState(buffer);
+    printf("%s", buffer);
+}
 void MemoryWrite(uint16_t address, uint8_t value) {
     memory[address] = value;
 }
 
-char buffer[256];
 
 int main() {
-    mos6502 mpu = mos6502(MemoryRead, MemoryWrite);
+    mos6502 mpu = mos6502(MemoryRead, MemoryWrite, ClockCycle);
     init_memory();
     mpu.Reset();
+    mpu.RunEternally();
     mpu.PrintState(buffer);
     printf("%s", buffer);
     return 0;
